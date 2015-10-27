ad20825b02080afaae52d75817352cc0bd0be192
Jonathan Willis <jonathanwillis@linux.com>
2015-10-27 06:51:39 -0400

    First lab: Pong

---
+++ pong1/gulpfile.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var gulp = require('gulp'),
+    del = require('del'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    replace = require('gulp-replace'),
+    sourcemaps = require('gulp-sourcemaps'),
+    uglify = require('gulp-uglify'),
+    browserSync = require('browser-sync').create(),
+    minifyCss = require('gulp-minify-css'),
+    concatCss = require('gulp-concat-css'),
+    size = require('gulp-size'),
+    util = require('gulp-util'),
+    eslint = require('gulp-eslint');
+
+gulp.task('default', ['build']);
+
+gulp.task('build', ['lint']);
+
+gulp.task('lint', function(){
+    var options = {
+        configFile: 'eslintrc.yml'
+    };
+    return gulp.src(['src/js/**/*.js'])
+        .pipe(eslint(options))
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
+});
+
+gulp.task('serve', [], function() {
+    browserSync.init({
+        server: './public',
+        files: ['public/**/*'],
+        open: false
+    });
+});
+
+gulp.task('test', function() {
+    util.log('@TODO');
+});
